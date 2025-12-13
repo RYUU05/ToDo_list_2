75cb9cd16ec6939368454dea3529701c37813acc
Nursultan Nazarbay <nursultan@MacBook-Air-Nursultan.local>
2025-12-13 20:49:53 +0500

    added block logic and nav bar

---
+++ lib/features/crypto_list/block/crypto_list_state.dart
@@ -0,0 +1,10 @@
+part of 'crypto_list_block.dart';
+
+class CryptoListState {}
+
+class CryptoListInitial extends CryptoListState {}
+
+class CryptoListLoaded extends CryptoListState {
+  CryptoListLoaded({required this.coinsList});
+  final List<CryptoCoin> coinsList;
+}
--- lib/features/crypto_list/screens/crypto_list_screen.dart
+++ lib/features/crypto_list/screens/crypto_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_practice/features/crypto_list/block/crypto_list_block.dart';
 import 'package:flutter_practice/features/crypto_list/widgets/widgets.dart';
-import 'package:flutter_practice/repositories/crypto_coin/crypto_coins_repositories.dart';
-import 'package:flutter_practice/repositories/crypto_coin/models/crypto_coin.dart';
 
 class CryptoListScreen extends StatefulWidget {
   const CryptoListScreen({super.key});
@@ -11,7 +11,13 @@ class CryptoListScreen extends StatefulWidget {
 }
 
 class _CryptoListScreenState extends State<CryptoListScreen> {
-  List<CryptoCoin>? _cryptoCoinsList;
+  final _cryptoListBlock = CryptoListBlock();
+  int currentIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    _cryptoListBlock.add(LoadCryptoList());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,24 +30,51 @@ class _CryptoListScreenState extends State<CryptoListScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: (_cryptoCoinsList == null)
-          ? SizedBox()
-          : ListView.separated(
-              separatorBuilder: (context, index) => Divider(),
-              itemCount: _cryptoCoinsList!.length,
-              itemBuilder: (context, index) {
-                final coin = _cryptoCoinsList![index];
-                return CryptoCoinTile(coin: coin);
-              },
-            ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          _cryptoCoinsList = await CryptoCoinsRepositories().getCoinsList();
-          setState(() {});
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _cryptoListBlock.add(LoadCryptoList());
         },
-        backgroundColor: Colors.yellow,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-        child: Icon(Icons.add),
+        child: BlocBuilder<CryptoListBlock, CryptoListState>(
+          bloc: _cryptoListBlock,
+          builder: (context, state) {
+            if (state is CryptoListLoaded) {
+              return ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: state.coinsList.length,
+                itemBuilder: (context, index) {
+                  final coin = state.coinsList[index];
+                  return CryptoCoinTile(coin: coin);
+                },
+              );
+            }
+            return Center(child: CircularProgressIndicator());
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 31, 31, 31),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          if (index == 1) {
+            Navigator.pushNamed(context, '/favorites');
+          }
+          if (index == 2) {
+            Navigator.pushNamed(context, '/settings');
+          }
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'List'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
