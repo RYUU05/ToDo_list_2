9bfb703fe3bbbf361ad9698f80042512c8b90ed6
Mukagal <amankosovmukagali1488@gmail.com>
2025-12-16 14:33:20 +0500

    Firebase configurations

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,10 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(const CryptoCurrienciesListApp());
 }
